e141abb33f915b3821709fbddf5cb1e90229fb9f
Pineapple <finnaggarwal@gmail.com>
2025-01-16 15:04:47 +0000

    options screen and finish enemy3

---
+++ main/game_objects/enemy3_death.go
@@ -0,0 +1,21 @@
+components {
+  id: "enemy"
+  component: "/main/scripts/enemy_death.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"anim\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/game_images/enemy_3_death.tilesource\"\n"
+  "}\n"
+  ""
+}
+embedded_components {
+  id: "sound"
+  type: "sound"
+  data: "sound: \"/game resources/audio/sfx/explosionCrunch_000.ogg\"\n"
+  ""
+}
